4d766a9815cccee49afebebdeb9dcff6e30b967a
Arc Trade <project4855@gmail.com>
2026-05-17 17:40:08 +0700

    fix: đổi 'Get Free USDC' thành 'Testnet Faucet' tránh Google flag phishing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
           rel="noreferrer"
           className="flex items-center gap-1.5 px-3 py-1.5 rounded-lg bg-green-500/10 border border-green-500/30 text-green-400 hover:bg-green-500/20 hover:text-green-300 transition-all text-xs font-medium"
         >
-          <span>💧</span> Get Free USDC
+          <span>💧</span> Testnet Faucet
         </a>
       </div>
 
